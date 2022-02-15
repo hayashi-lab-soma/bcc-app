5083d569f1c908774afdf565d3f37d32696999a9
f-tominaga <tommy.tommy.walker@gmail.com>
2022-02-16 00:35:22 +0900

    fetch with amplify staging branch

---
+++ src/ui-components/MainNavBar.jsx
@@ -0,0 +1,68 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, Icon, Text } from "@aws-amplify/ui-react";
+export default function MainNavBar(props) {
+  const { overrides: overridesProp, ...rest } = props;
+  const overrides = { ...overridesProp };
+  return (
+    <Flex
+      gap="20px"
+      direction="row"
+      width="1440px"
+      alignItems="center"
+      position="relative"
+      padding="24px 32px 24px 32px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "Flex")}
+    >
+      <Flex
+        gap="10px"
+        direction="row"
+        height="24px"
+        justifyContent="center"
+        alignItems="center"
+        shrink="0"
+        alignSelf="stretch"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Flex.Flex[0]")}
+      >
+        <Icon
+          width="20px"
+          height="20px"
+          pathData="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM5.07 16.28C5.5 15.38 8.12 14.5 10 14.5C11.88 14.5 14.51 15.38 14.93 16.28C13.57 17.36 11.86 18 10 18C8.14 18 6.43 17.36 5.07 16.28ZM16.36 14.83C14.93 13.09 11.46 12.5 10 12.5C8.54 12.5 5.07 13.09 3.64 14.83C2.62 13.49 2 11.82 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 11.82 17.38 13.49 16.36 14.83L16.36 14.83ZM10 4C8.06 4 6.5 5.56 6.5 7.5C6.5 9.44 8.06 11 10 11C11.94 11 13.5 9.44 13.5 7.5C13.5 5.56 11.94 4 10 4ZM10 9C9.17 9 8.5 8.33 8.5 7.5C8.5 6.67 9.17 6 10 6C10.83 6 11.5 6.67 11.5 7.5C11.5 8.33 10.83 9 10 9Z"
+          viewBox={{ minX: 0, minY: 0, width: 20, height: 20 }}
+          color="rgba(13.000000175088644,26.000000350177288,38.0000015348196,1)"
+          shrink="0"
+          position="relative"
+          {...getOverrideProps(overrides, "Flex.Flex[0].Icon[0]")}
+        ></Icon>
+        <Text
+          fontFamily="Inter"
+          fontSize="20px"
+          fontWeight="400"
+          color="rgba(0,0,0,1)"
+          textTransform="capitalize"
+          lineHeight="23.4375px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          children="UserName"
+          {...getOverrideProps(overrides, "Flex.Flex[0].Text[0]")}
+        ></Text>
+      </Flex>
+    </Flex>
+  );
+}
