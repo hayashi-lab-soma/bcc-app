5213b14df66d02bb3708996707d7641829a97f71
f-tominaga <tommy.tommy.walker@gmail.com>
2022-02-16 01:15:57 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css';
-import {Amplify, Auth} from 'aws-amplify';
+// import {Amplify, Auth} from 'aws-amplify';
 import {withAuthenticator,} from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css';
-import awsconfig from './aws-exports';
 
-Amplify.configure(awsconfig);
+// import awsconfig from './aws-exports';
+
+// Amplify.configure(awsconfig);
 
 function App() {
   return (
