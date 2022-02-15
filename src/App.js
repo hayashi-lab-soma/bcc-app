a2c0bf8df84f2d809f2ffd44d34573e4b2a763e1
f-tominaga <tommy.tommy.walker@gmail.com>
2022-02-16 01:11:18 +0900

    add ui css files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './App.css';
-import Amplify from 'aws-amplify';
+import {Amplify, Auth} from 'aws-amplify';
 import {withAuthenticator,} from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
