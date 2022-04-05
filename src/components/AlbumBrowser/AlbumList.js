3452b621b22bd69390b60a4bc34800a103905af5
f-tominaga <tommy.tommy.walker@gmail.com>
2022-04-05 11:10:08 +0900

    change Album card UI

--- src/components/AlbumBrowser/AlbumList.js
+++ src/components/AlbumBrowser/AlbumList.js
@@ -1,6 +1,10 @@
-import React, { useState, useEffect } from 'react'
-import { Box, Grid, Stack, Typography, Button, CardHeader } from '@mui/material'
-import { Card, CardActionArea, CardContent, CardActions } from '@mui/material'
+import React from 'react'
+import { Box, Stack, Button, IconButton, CardHeader, CardContent } from '@mui/material'
+import { Card, CardActionArea, CardActions } from '@mui/material'
+
+import PhotoLibraryOutlinedIcon from '@mui/icons-material/PhotoLibraryOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { minHeight } from '@mui/system';
 
 // props
 // album: album object
@@ -10,23 +14,51 @@ const Album = (props) => {
   return (
     <Card
       key={props.album.id}
+      sx={{ minWidth: 180 }}
     >
-      <CardHeader
-        title={props.album.name}
-      />
-      <CardActionArea onClick={() => { props.onClick(props.album) }} >
+
+      <CardActionArea
+        onClick={() => { props.onClick(props.album) }} >
         <CardContent>
-          <Typography >{props.album.name}</Typography>
+          <Box
+            sx={{ display: 'flex' }}
+            justifyContent={'center'}>
+            <PhotoLibraryOutlinedIcon fontSize='large' />
+          </Box>
         </CardContent>
       </CardActionArea>
 
-      {
+      <CardHeader
+        title={props.album.name}
+        subheader={
+          "-----"
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        sx={{minHeight: 80}}
+      />
+
+
+
+      {/* <CardActionArea onClick={() => { props.onClick(props.album) }} >
+        <CardContent>
+          <Typography >{props.album.name}</Typography>
+        </CardContent>
+      </CardActionArea> */}
+
+      {/* {
         props.album.id !== 'all' &&
         props.album.id !== 'non' &&
         <CardActions>
-          <Button onClick={() => { props.onDelete(props.album) }}>削除</Button>
+          <Box
+            sx={{ display: 'flex', flexDirection: 'row' }}>
+            <Button variant='contained' onClick={() => { props.onDelete(props.album) }}>削除</Button>
+          </Box>
         </CardActions>
-      }
+      } */}
 
     </Card>
   )
