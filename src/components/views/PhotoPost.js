d31da2a1a82596a66ae7d6eef0a59d1637e72c2a
f-tominaga <tommy.tommy.walker@gmail.com>
2023-02-24 10:04:59 +0900

    add uuid package

--- src/components/views/PhotoPost.js
+++ src/components/views/PhotoPost.js
@@ -1,13 +1,10 @@
-import React, { useState, useRef } from 'react'
-
-import { Box, Fab, } from '@mui/material'
-import { DropzoneArea } from 'react-mui-dropzone'
+import React, { useState } from 'react'
 
 import { PhotoPostDialog, PhotoPostedDialog } from '../templates'
-
 import { Storage } from 'aws-amplify'
-
+import { Fab, } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
+import { v4 as uuidv4 } from 'uuid'
 
 const PhotoPost = (props) => {
   // const files = useRef([])
@@ -55,13 +52,16 @@ const PhotoPost = (props) => {
 
           // props.onSend(files)
           await Promise.all(files.map(async (file) => {
-            const fname = file.name.split('.').shift() //get file name without explanation
-            const type = file.name.split('.').pop()
-            const date = new Date()
-            const key = `${fname}_${date.getTime()}.${type}` //key (file object name) is fname + UTC + .jpg
+            //const fname = file.name.split('.').shift() //get file name without explanation
+            //const type = file.name.split('.').pop()
+            //const date = new Date()
+            //const key = `${fname}_${date.getTime()}.${type}` //key (file object name) is fname + UTC + .jpg
+            
+            const fname = file.name.split('.').shift()
+            const key = `'${fname}'${uuidv4()}.jpg`
             try {
               const ret = await Storage.put(key, file, { level: 'private' })
-              console.log(ret)
+              // console.log(ret)
             }
             catch (e) {
               console.error(e)
